8522e9cd01aef3fb9b19363fc6ccd150df3d2478
anjana wimalasena <anjana.wimalasena@gmail.com>
2021-02-16 18:45:49 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import { BrowserRouter, Switch,Route} from 'react-router-dom';
+import Home from './components/Home/Home'
+import TeacherR from './components/Register/TeacherR';
+import StudentR2 from './components/Register/StudentR2';
+import AccSTmenu from './components/UserProfile/StudentP/AccSTmenu';
+import AccSTsubscriptions from './components/UserProfile/StudentP/AccSTsubscriptions';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import AccTCRaddnew from './components/UserProfile/TeacherP/AccTCRaddnew';
+import AccTCRpublished from './components/UserProfile/TeacherP/AccTCRpublished';
+
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+       
+         <Switch>
+
+            <Route path="/" exact component={Home} />
+            <Route path="/teacherR"  component={TeacherR} />
+            <Route path="/studentR2"  component={StudentR2} />
+            <Route path='/accSTsubscriptions'  component={AccSTsubscriptions} />
+            <Route path="/accSTmenu"  component={AccSTmenu} />
+            <Route path="/home"  component={Home} />
+            <Route path="/about"  component={About} />
+            <Route path="/contact"  component={ Contact} />
+            <Route path='/AccTCRaddnew'  component={AccTCRaddnew} />
+            <Route path='/AccTCRpublished'  component={AccTCRpublished} />
+
+
+       </Switch>
+
+    </div>
+    </BrowserRouter> 
+        
+        
+  );
+}
+
+export default App;
+
+
+ 
